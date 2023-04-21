a0a17d2586c8935e45ec003b41f553303c2c17ac
Amal Nadifi <amal.nadifi9@gmail.com>
2023-04-21 13:34:05 -0700

    Task5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The program prints all single numbers of base 10
+ * starting from 0, followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 0 ; x <= 10 ; x++)
+{
+	printf("%d", x);
+}
+printf("\n");
+return (0);
+}
