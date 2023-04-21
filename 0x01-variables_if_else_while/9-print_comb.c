d243ee1ffafcf0d9a95b0e18620fe270a0154add
Amal Nadifi <amal.nadifi9@gmail.com>
2023-04-21 14:22:41 -0700

    Task9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - The program prints all single digit numbers of base 10
+ * starting from 0, followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 48 ; x < 58 ; x++)
+{
+	putchar(x);
+	if (x != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
