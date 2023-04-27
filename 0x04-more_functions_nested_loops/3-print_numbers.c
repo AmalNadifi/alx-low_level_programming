739b25029d545053a6d6f89441e748e32788e44a
Amal Nadifi <amal.nadifi9@gmail.com>
2023-04-27 09:33:03 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - The program prints the numbers from 0 to 9
+ * followed by a new line
+ * Return: numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
