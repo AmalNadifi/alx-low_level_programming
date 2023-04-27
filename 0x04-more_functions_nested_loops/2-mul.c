fcd389d4fbaaec681996d0594d96bf7860879e76
Amal Nadifi <amal.nadifi9@gmail.com>
2023-04-27 09:09:43 -0700

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - The program computes the absolute value of an integer
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the multiplication result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
