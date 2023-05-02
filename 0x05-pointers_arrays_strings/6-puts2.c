eb86028ad856cce517e93e5997065e7298aa66d6
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-02 16:11:37 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - the fucntion prints every other character of a string
+ * followed by a new line
+ * @str: the string that will be printed
+ * Return: no return value
+ */
+
+void puts2(char *str)
+{
+	int  a;
+	int b = 0;
+
+	do {
+		b++;
+	} while (str[b] != '\0');
+
+	for (a = 0; a < b; a = a + 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
