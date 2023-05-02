decdc9433f48466b50b0dc33c596bd8ee397ca28
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-02 15:41:17 -0700

    Task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string in reverse
+ * followed by a new line
+ * @s: the string to be printed
+ * Return: no return value
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '0')
+	{
+		a++;
+	}
+	for (a = a - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
