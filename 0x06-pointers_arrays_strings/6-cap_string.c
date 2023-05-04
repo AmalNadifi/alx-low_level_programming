9562ec4b4beef32bfe825fc656452a4f2824b0cf
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-04 10:01:01 -0700

    Task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *cap_string - the function capitalizes all words of a string
+ * @a: string to capitalize all its words
+ * Return: 0 (success)
+ */
+
+char *cap_string(char *a)
+{
+	int x;
+
+	while (a[x])
+	{
+		for (x = 0; a[x] >= 'a' && a[x] <= 'z'; x++)
+		{
+			if (a[x - 1] == '' || a[x - 1] == '\t' ||
+					a[x - 1] == '\n' || a[x - 1] == ',' ||
+					a[x - 1] == ';' || a[x - 1] == '.' ||
+					a[x - 1] == '!' || a[x - 1] == '?' ||
+					a[x - 1] == '"' || a[x - 1] == '(' ||
+					a[x - 1] == ')' || a[x - 1] == '{' ||
+					a[x - 1] == '}' || x == 0)
+				a[x] = a[x] - 32;
+		}
+		x++;
+	}
+	return (a);
+}
