5d19d9c2639efcf7a9d567345646a3612dcb3520
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-10 15:36:01 -0700

    Task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - the function returns the natural square root of a number
+ * @n: the integer to calculate its square root
+ * Return: the result as an integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt_natural(n, 1));
+}
+
+/**
+ * square_checker - check if the square root is natural
+ * @n: the integer to calculate its square root
+ * @sqr: the square of our integer n
+ * Return: the result as an integer
+ */
+
+int square_checker(int n, int sqr)
+{
+	if (sqr * sqr == n)
+		return (sqr);
+	else if (sqr * sqr < n)
+		return (square_checker(n, sqr + 1));
+	else
+		return (-1);
+}
