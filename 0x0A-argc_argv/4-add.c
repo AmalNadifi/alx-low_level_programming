879e3743c3e2567beaa537ebb9bb82df89d45696
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-12 06:21:44 -0700

    Task4_updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,23 +17,13 @@ int main(int argc, char **argv)
 	int res = 0;
 	char *sub;
 
-	if (argc == 0)
-	{
-		printf("0\n");
-	}
 	while (--argc)
 	{
 		for (sub = argv[argc]; *sub; sub++)
-		{
 			if (*sub < '0' || *sub > '9')
-			{
 				return (printf("Error\n"), 1);
-			}
-			res = res + atoi(argv[argc]);
-		}
+		res += atoi(argv[argc]);
 	}
 	printf("%d\n", res);
 	return (0);
 }
-
-
