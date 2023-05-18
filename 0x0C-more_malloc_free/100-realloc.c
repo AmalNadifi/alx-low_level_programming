ad401bf352ea349ce1dd6843e3cc3942a3c0509e
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-18 10:08:58 -0700

    Task4_updated_3.0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		res = malloc(new_size);
-		if (ptr == NULL)
+		if (res == NULL)
 		{
 			return (NULL);
 		}
