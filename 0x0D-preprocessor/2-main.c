27d6c695e5955aae38c9dd67a07f5064dd95e035
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-22 06:59:41 -0700

    Task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * main - the program prints the name of the file it was compiled from,
+ * followed by a new line
+ * Return: 0 ( SUccess)
+ */
+
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
