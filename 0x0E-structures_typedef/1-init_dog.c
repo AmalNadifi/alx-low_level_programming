1142e519dd118d9698e49560e25cada05b88bf6f
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-22 07:37:31 -0700

    Task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - the function initializes a variable of type struct dog
+ * @d: dog to initialize
+ * @name: poppy's name
+ * @age: poppy's age
+ * @owner: poppy's owner name
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name =name;
+		d->age =age;
+		d->owner =owner;
+	}
