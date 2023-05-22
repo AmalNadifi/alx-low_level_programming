e0c5f68999a3160e45a67a3533740f0ea6ff5019
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-22 08:32:38 -0700

    Task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - the function writes a struct dog
+ * @d: the poppy to print
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %n", d->owner ? d->owner : "(nil)");
+	}
+}
