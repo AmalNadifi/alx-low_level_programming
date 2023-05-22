38e54b3ecd628044043268300850f11a7c6bcb51
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-22 10:18:21 -0700

    Task2_UPDATED

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,4 +15,5 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 		printf("Owner: %n", d->owner ? d->owner : "(nil)");
 	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
