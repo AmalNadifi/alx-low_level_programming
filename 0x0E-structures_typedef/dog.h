30fb3223d3a9a927d1e4ba43608359b939f0ff7e
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-22 07:28:36 -0700

    Task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a poppy struct with three elements
+ * @name: poppy's name
+ * @age: poppy's age
+ * @owner: poppy's owner name
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
