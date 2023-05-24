c0108c50726df24e6d5ace7890466fb9e1219979
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-24 08:34:39 -0700

    Task3_updated_2.0

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,15 +17,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int x = 0;
 
-	while (i < 5)
+	while (x < 5)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
+		if (s && s[0] == ops[x].op[0] && !s[1])
 		{
-			return (ops[i].f);
+			return (ops[x].f);
 		}
-		i++;
+		x++;
 	}
 
 	return (0);
