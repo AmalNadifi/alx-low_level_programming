b609fb2e106dae80d0e3778bb66faddf1f20ad4e
Amal Nadifi <amal.nadifi9@gmail.com>
2023-05-24 10:31:23 -0700

    Task4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the program prints the opcodes
+ * @argv: the vextor of arguments
+ * @argc: the counter of arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	char *ptr = (char *)main;
+	int num_b;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_b = atoi(argv[1]);
+	if (num_b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	do {
+		printf("%02hhx%s", *ptr++, num_b ? " " : "\n");
+	} while (num_b--);
+	return (NULL);
+}
