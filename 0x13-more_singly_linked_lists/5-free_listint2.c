37ca0dd355480a14dad8f42fff90cf0cf8e1aabd
AmalNadifi <amal.nadifi9@gmail.com>
2023-06-05 10:23:28 -0700

    Task5_updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *my_tmp2;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		my_tmp2 = head;
-		head = head->next;
+		my_tmp2 = *head;
+		*head = *head->next;
 		free(my_tmp2);
 	}
 	*head = NULL;
