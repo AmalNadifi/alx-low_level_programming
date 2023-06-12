2f360f522a5db4864b487e8bf5edc0a9dde5e4ea
AmalNadifi <amal.nadifi9@gmail.com>
2023-06-12 14:24:51 -0700

    Task3_updated_5.0

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -38,6 +38,7 @@ int main(int argc, char **argv)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 					argv[1]), exit(98);
+		}
 	}
 	if (close(src) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src), exit(100);
