e1736b30cef4461b2d30538b1fc16106719a3207
AmalNadifi <amal.nadifi9@gmail.com>
2023-06-12 09:18:00 -0700

    task1_updated_3.0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		text_content = "";
+		text_content = " ";
 	}
 	for (idx = 0; text_content[idx] != '\0'; idx++)
 
