87f9dc01086e37797df56202fcbdfc775739a630
AmalNadifi <amal.nadifi9@gmail.com>
2023-06-12 09:22:46 -0700

    task1_updated5.0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		do {
 			idx++;
-		} while (text_content[idx] != '\0';
+		} while (text_content[idx] != '\0');
 	}
 
 	o_var = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
