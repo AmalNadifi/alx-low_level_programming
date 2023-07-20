a3c22922bee4a4a9e87dff0f079620278406e0d4
AmalNadifi <amal.nadifi9gmail.com>
2023-07-20 11:43:24 -0700

    task3_updated_2.0

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,6 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
+	last_node = *head;
 	while (last_node->next != NULL)
 		last_node = last_node->next;
 	new_node->prev = last_node;
